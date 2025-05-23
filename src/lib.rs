d5062c06a17eed2f92ecdcb7d32fdc18c7f5b158
Alex T <aletapia@proton.me>
2025-05-22 23:47:13 -0600

    feat: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,92 +11,93 @@ pub const USER_ADDRESS: &str = "0x53Dee653941345fC1241444F7b1E7dA3beC73Aab";
 pub const INITIAL_BALANCE: f64 = 47_300.0;
 
 pub struct CryptoClient {
-    user_address: Address,
-    hl_client: InfoClient,
+	user_address: Address,
+	hl_client: InfoClient,
 }
 
 impl CryptoClient {
-    pub async fn build() -> Result<Self> {
-        let hl_client = InfoClient::new(None, Some(BaseUrl::Mainnet)).await?;
-        Ok(Self {
-            user_address: USER_ADDRESS.parse()?,
-            hl_client,
-        })
-    }
+	pub async fn build() -> Result<Self> {
+		let hl_client = InfoClient::new(None, Some(BaseUrl::Mainnet)).await?;
+		Ok(Self {
+			user_address: USER_ADDRESS.parse()?,
+			hl_client,
+		})
+	}
 
-    pub async fn fetch_perp_acct_value(&self) -> Result<f64> {
-        let res = self.hl_client.user_state(self.user_address).await?;
+	pub async fn fetch_perp_acct_value(&self) -> Result<f64> {
+		let res = self.hl_client.user_state(self.user_address).await?;
 
-        let acct_val = res.cross_margin_summary.account_value;
-        let acct_val = acct_val.parse::<f64>()?;
+		let acct_val = res.cross_margin_summary.account_value;
+		let acct_val = acct_val.parse::<f64>()?;
 
-        Ok(acct_val)
-    }
+		Ok(acct_val)
+	}
 
-    pub async fn fetch_spot_acct_value(&self) -> Result<f64> {
-        let (res, mids) = tokio::join!(
-            self.hl_client.user_token_balances(self.user_address),
-            self.get_all_mids()
-        );
-        let mids = mids?;
+	pub async fn fetch_spot_acct_value(&self) -> Result<f64> {
+		let (res, mids) = tokio::join!(
+			self.hl_client.user_token_balances(self.user_address),
+			self.get_all_mids()
+		);
+		let mids = mids?;
 
-        let mut acct_val = 0.0;
+		let mut acct_val = 0.0;
 
-        for token_bal in res?.balances {
-            if token_bal.coin == "USDC" {
-                acct_val += token_bal.total.parse::<f64>()?;
-                continue;
-            }
-            let token_balance = token_bal.total.parse::<f64>()?;
-            let token_price = mids
-                .get(&token_bal.coin)
-                .with_context(|| format!("Token {} not found in mids", token_bal.coin))
-                .and_then(|p| {
-                    p.parse::<f64>().context("Failed to parse token price into a f64")
-                })?;
-            acct_val += token_balance * token_price;
-        }
+		for token_bal in res?.balances {
+			if token_bal.coin == "USDC" {
+				acct_val += token_bal.total.parse::<f64>()?;
+				continue;
+			}
+			let token_balance = token_bal.total.parse::<f64>()?;
+			let token_price = mids
+				.get(&token_bal.coin)
+				.with_context(|| format!("Token {} not found in mids", token_bal.coin))
+				.and_then(|p| {
+					p.parse::<f64>()
+						.context("Failed to parse token price into a f64")
+				})?;
+			acct_val += token_balance * token_price;
+		}
 
-        Ok(acct_val)
-    }
+		Ok(acct_val)
+	}
 
-    async fn get_all_mids(&self) -> Result<HashMap<String, String>> {
-        let res = self.hl_client.all_mids().await?;
-        Ok(res)
-    }
+	async fn get_all_mids(&self) -> Result<HashMap<String, String>> {
+		let res = self.hl_client.all_mids().await?;
+		Ok(res)
+	}
 
-    pub async fn calculate_fr_open_pos(&self) -> Result<f64> {
-        let res = self
-            .hl_client
-            .http_client
-            .post(
-                "/info",
-                json!({
-                    "type": "clearinghouseState",
-                    "user": self.user_address,
-                })
-                .to_string(),
-            )
-            .await?;
-        let res: Value = serde_json::from_str(&res)?;
+	pub async fn calculate_fr_open_pos(&self) -> Result<f64> {
+		let res = self
+			.hl_client
+			.http_client
+			.post(
+				"/info",
+				json!({
+					"type": "clearinghouseState",
+					"user": self.user_address,
+				})
+				.to_string(),
+			)
+			.await?;
+		let res: Value = serde_json::from_str(&res)?;
 
-        let mut cum_funding = 0.0;
+		let mut cum_funding = 0.0;
 
-        if let Some(asset_positions) =
-            res.get("assetPositions").and_then(|ap| ap.as_array())
-        {
-            for ap_value in asset_positions {
-                if let Some(all_time_str) = ap_value
-                    .get("position")
-                    .and_then(|p| p.get("cumFunding"))
-                    .and_then(|cf| cf.get("allTime"))
-                    .and_then(|at| at.as_str())
-                {
-                    cum_funding += all_time_str.parse::<f64>().unwrap_or(0.0);
-                }
-            }
-        }
+		if let Some(asset_positions) =
+			res.get("assetPositions").and_then(|ap| ap.as_array())
+		{
+			for ap_value in asset_positions {
+				if let Some(all_time_str) = ap_value
+					.get("position")
+					.and_then(|p| p.get("cumFunding"))
+					.and_then(|cf| cf.get("allTime"))
+					.and_then(|at| at.as_str())
+				{
+					cum_funding += all_time_str.parse::<f64>().unwrap_or(0.0);
+				}
+			}
+		}
 
-        Ok(cum_funding.neg())
-    }
+		Ok(cum_funding.neg())
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use anyhow::Result;
 use cryprice::*;
+use env_logger_wrapper::new_builder;
 use log::LevelFilter;
-use thin_logger::ThinLogger;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    ThinLogger::new(LevelFilter::Debug)
-        .external_logs(LevelFilter::Info)
-        .init()
-        .ok();
+	new_builder(LevelFilter::Debug).init();
 
-    let client = CryptoClient::build().await?;
+	let client = CryptoClient::build().await?;
 
-    let (perp, spot, fr) = tokio::try_join!(
-        client.fetch_perp_acct_value(),
-        client.fetch_spot_acct_value(),
-        client.calculate_fr_open_pos()
-    )?;
+	let (perp, spot, fr) = tokio::try_join!(
+		client.fetch_perp_acct_value(),
+		client.fetch_spot_acct_value(),
+		client.calculate_fr_open_pos()
+	)?;
 
-    let total = perp + spot;
+	let total = perp + spot;
 
-    let pnl = total - INITIAL_BALANCE;
-    let pnl_pct = pnl / INITIAL_BALANCE * 100.0;
+	let pnl = total - INITIAL_BALANCE;
+	let pnl_pct = pnl / INITIAL_BALANCE * 100.0;
 
-    print_table(perp, spot, total, pnl, pnl_pct);
+	print_table(perp, spot, total, pnl, pnl_pct);
 
-    log::info!("fr: {}", fr);
+	log::info!("fr: {}", fr);
 
-    Ok(())
+	Ok(())
 }
